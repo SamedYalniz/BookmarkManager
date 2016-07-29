dab952be318d11261bd6765e55a955d089381139
Samed Yalniz <s.yalniz@hotmail.de>
2016-07-29 18:20:20 +0200

    Ch#22 duplicate registrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User
   include DataMapper::Resource
 
   property :id, Serial
-  property :email, String, required: true, format: :email_address
+  property :email, String, required: true, format: :email_address, unique: true 
   property :password_encrypted, String, length: 60
   attr_accessor :password_confirmation
   attr_reader   :password
--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -18,7 +18,7 @@ feature 'sign up' do
     click_button 'Sign up'
     expect(User.count).to eq 0
     expect(current_path).to eq '/users'
-    expect(page).to have_content 'Password and confirmation password do not match'
+    expect(page).to have_content 'Password does not match the confirmation'
   end
 
   scenario 'cant sign up with a blank email address' do
@@ -37,5 +37,19 @@ feature 'sign up' do
     fill_in 'password_confirmation', with: 'password123'
     click_button 'Sign up'
     expect(User.count).to eq 0
+  end
+  scenario 'user cannot sign up with an existing email address' do
+    visit '/users/new'
+    fill_in 'email', with: 's.yalniz@hotmail.de'
+    fill_in 'password', with: 'password123'
+    fill_in 'password_confirmation', with: 'password123'
+    click_button 'Sign up'
+    visit '/users/new'
+    fill_in 'email', with: 's.yalniz@hotmail.de'
+    fill_in 'password', with: 'password123'
+    fill_in 'password_confirmation', with: 'password123'
+    click_button 'Sign up'
+    expect(User.count).to eq 1
+    expect(page).to have_content("Email is already taken")
   end
 end
